545d0ce58f9898570bd6a2c81d26652d1d00c8a0
neuberfran <neuberfran@gmail.com>
2021-05-31 15:12:55 -0300

    second commit

--- functions/index.js
+++ functions/index.js
@@ -2,45 +2,45 @@ const functions = require("firebase-functions");
 const { firestore } = require("./admin");
 
 exports.updateGarage = functions.firestore
-  .document("device-configs/garagem2")
-  .onUpdate((change, context) => {
-    // Get an object representing the document
-    // e.g. {'name': 'Marie', 'age': 66}
-    const newValue = change.after.data();
+    .document("device-configs/garagem2")
+    .onUpdate((change, context) => {
 
-    const garagestatev = newValue.garagestate;
+        //Get the latest value after update
+        const newValue = change.after.data();
 
-      firestore
-      .doc(`device-configs/garagem`)
-      .get()
-      .then((areaSnapshot) => {
+        //Current state of garage
+        const garagestatev = newValue.garagestate;
 
-        const targetDoc = areaSnapshot.data().value.oipenPercent
+        return firestore.doc(`device-configs/garagem`).get()
+            .then((areaSnapshot) => areaSnapshot.data().value.openPercent)
+            .then(targetDoc => {
 
-            if (garagestatev) {
-                console.log("garagestate is true = targetDodc=k"+targetDoc);
-      
-                if (targetDoc == 100) {
-                  console.log("vou mandar 0");
-      
-                  firestore
-                    .collection("device-confings")
-                    .document("garagem")
-                    .update(mapOf("value.openPercent", to, 0));
+                //Target doc has openPercent of other document ('garagem')
+                if (garagestatev) {
+                    console.log("garagestate is true = targetDodc" + targetDoc);
+
+                    if (targetDoc === 100) {
+                        console.log("vou mandar 0");
+                        //Returning Promise inside of .then() block 
+                        return firestore
+                            .collection("device-configs")
+                            .document("garagem")
+                            .update({ "value.openPercent": 0 });
+                    }
+
+                } else {
+
+                    console.log("garagestate is false = targetDodc" + targetDoc);
+
+                    if (targetDoc === 0) {
+                        console.log("vou mandar 100");
+                        //Returning Promise inside of .then() block 
+                        return firestore
+                            .collection("device-configs")
+                            .document("garagem")
+                            .update({ "value.openPercent": 100 });
+                    }
                 }
+            })
 
-            } else if (!garagestatev) {
-                
-                console.log("garagestate is false = targetDodc=k"+targetDoc);
-
-                if (targetDoc == 0) {
-                  console.log("vou mandar 100");
-                  firestore
-                    .collection("device-confings")
-                    .document("garagem")
-                    .update(mapOf("value.openPercent", to, 100));
-                }
-              }
-   
-      });
-  });
+    });
